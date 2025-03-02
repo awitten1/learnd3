5fd605b4fc4676f5b73392cfbb210c9d78a43e46
Andrew Witten <awitten007@gmail.com>
2025-03-01 19:01:52 -0500

    random

--- src/main.js
+++ src/main.js
@@ -1,37 +1,79 @@
+
+
 import * as d3 from 'd3';
 
-// Declare the chart dimensions and margins.
-const width = 640;
-const height = 400;
-const marginTop = 20;
-const marginRight = 20;
-const marginBottom = 30;
-const marginLeft = 40;
 
-// Declare the x (horizontal position) scale.
-const x = d3.scaleUtc()
-    .domain([new Date("2023-01-01"), new Date("2024-01-01")])
-    .range([marginLeft, width - marginRight]);
+const chart = (data) => {
+  // Declare the chart dimensions and margins.
+  const width = 928;
+  const height = 500;
+  const marginTop = 30;
+  const marginRight = 0;
+  const marginBottom = 30;
+  const marginLeft = 40;
 
-// Declare the y (vertical position) scale.
-const y = d3.scaleLinear()
-    .domain([0, 100])
-    .range([height - marginBottom, marginTop]);
+  console.log(Array.from(d3.group(data, (d) => d.letter).keys()).sort())
+  // Declare the x (horizontal position) scale.
+  const x = d3.scaleBand()
+      //.domain(d3.groupSort(data, ([d]) => -d.frequency, (d) => d.letter))
+      .domain(Array.from(d3.group(data, (d) => d.letter).keys()).sort())
+      .range([marginLeft, width - marginRight])
+      .padding(0.1);
+  
+  // Declare the y (vertical position) scale.
+  const y = d3.scaleLinear()
+      .domain([0, d3.max(data, (d) => d.frequency)])
+      .range([height - marginBottom, marginTop]);
 
-// Create the SVG container.
-const svg = d3.create("svg")
-    .attr("width", width)
-    .attr("height", height);
+  // Create the SVG container.
+  const svg = d3.create("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("viewBox", [0, 0, width, height])
+      .attr("style", "max-width: 100%; height: auto;");
 
-// Add the x-axis.
-svg.append("g")
-    .attr("transform", `translate(0,${height - marginBottom})`)
-    .call(d3.axisBottom(x));
+  // Add a rect for each bar.
+  svg.append("g")
+      .attr("fill", "steelblue")
+    .selectAll()
+    .data(data)
+    .join("rect")
+      .attr("x", (d) => x(d.letter))
+      .attr("y", (d) => y(d.frequency))
+      .attr("height", (d) => y(0) - y(d.frequency))
+      .attr("width", x.bandwidth());
 
-// Add the y-axis.
-svg.append("g")
-    .attr("transform", `translate(${marginLeft},0)`)
-    .call(d3.axisLeft(y));
+  // Add the x-axis and label.
+  svg.append("g")
+      .attr("transform", `translate(0,${height - marginBottom})`)
+      .call(d3.axisBottom(x).tickSizeOuter(0));
 
-// Append the SVG element to the container
-document.getElementById("container").appendChild(svg.node());
+  // Add the y-axis and label, and remove the domain line.
+  svg.append("g")
+      .attr("transform", `translate(${marginLeft},0)`)
+      .call(d3.axisLeft(y).tickFormat((y) => (y * 100).toFixed()))
+      .call(g => g.select(".domain").remove())
+      .call(g => g.append("text")
+          .attr("x", -marginLeft)
+          .attr("y", 10)
+          .attr("fill", "currentColor")
+          .attr("text-anchor", "start")
+          .text("↑ Frequency (%)"));
+
+  // Return the SVG element.
+  return svg.node();
+
+}
+
+const A = 'A';
+const AValue = A.charCodeAt(0);
+const Z = 'Z'
+const ZValue = Z.charCodeAt(0)
+
+const data = []
+for (let i = AValue; i < ZValue; i++) {
+  const c = String.fromCharCode(i);
+  data.push({letter: c, frequency: Math.random()});
+} 
+
+container.append(chart(data))
